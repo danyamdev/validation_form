f03afbb57a90b03a717d6a4d429225df32b5cf48
danyamamin99 <50888464+danyamamin99@users.noreply.github.com>
2021-10-11 17:52:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const form = document.querySelector('.form');
 
+const removeClassElems = () => Array.from(form.elements).forEach(elem => elem.classList.remove('error'));
+
 const checkField = (elem) => {
     if (elem.name === 'fname' || elem.name === 'lname' || elem.name === 'tname') {
-        if (elem.value < 5) elem.classList.add('error')
+        if (elem.value.length < 5) elem.classList.add('error')
     }
 
-    if (elem.)
+    if (elem.name === 'password') {
+        if (elem.value.length < 5) elem.classList.add('error')
+    }
 }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     
+    removeClassElems();
+
     for (const elem of form.elements) {
         if (elem.tagName !== 'BUTTON') {
             checkField(elem)
